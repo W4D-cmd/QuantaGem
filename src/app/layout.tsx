d6d38b6bed36dca7d18f48670fcd8546c8089f61
W4D-cmd <69420009+W4D-cmd@users.noreply.github.com>
2025-06-05 14:38:21 +0200

    Minor UI adjustments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Roboto, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { HighlightJsThemeLoader } from "@/components/HighlightJsThemeLoader";
-
-const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" });
-
-const robotoMono = Roboto_Mono({
-  subsets: ["latin"],
-  variable: "--font-roboto-mono",
-});
 
 export const metadata: Metadata = {
   title: "QuantaGem",
@@ -23,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${roboto.variable} ${robotoMono.variable}`}>
+    <html lang="en">
       <body
         className={
           "antialiased bg-white dark:bg-neutral-950 text-black dark:text-white transition-colors duration-300 ease-in-out"
